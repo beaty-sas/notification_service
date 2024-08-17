52fe41ced8a98d084ad6d49c65276bb86b6976a3
SerhiiLakodei <>
2024-08-17 21:14:22 +0300

    rename

--- notification_service/clients/__init__.py
+++ notification_service/clients/__init__.py
@@ -1,6 +1,6 @@
 from notification_service.clients.bsg import BSGProvider
 from notification_service.clients.sns import AWSSNSClient
-from notification_service.clients.telegram import TelegramClient
+from notification_service.clients.tgram import TelegramClient
 
 providers = {
     'sns': AWSSNSClient,
